8af33d3ab5a42bc72540a9752dbdaac156565707
1nsanov <73361511+1nsanov@users.noreply.github.com>
2021-11-03 13:46:21 +0300

    Implemented 3 task

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var dropdownContainer = document.getElementsByClassName("dropdown-container")
 var i;
@@ -38,15 +39,12 @@ document.getElementById('nav-btn').addEventListener('click', function () {
 	if (document.getElementById('sidenav').style.display === 'block') {
 		document.getElementById('sidenav').style.display = 'none'
 		document.getElementById('leftAside').style.display = 'none'
-		console.log(0)
 	}
 	else {
 		document.getElementById('sidenav').style.display = 'block'
 		document.getElementById('leftAside').style.display = 'block'
-		console.log(1)
 	}
 })
-
 
 ChangeDisplayView();
 window.addEventListener('resize', ChangeDisplayView);
@@ -56,3 +54,54 @@ function ChangeDisplayView() {
 		document.querySelector('#leftAside').style.display = 'block'
 	}
 }
+
+
+faker.locale = 'ru';
+var listData = []
+ListFillArray(15, listData)
+CreateElementOption(listData)
+
+function ListFillArray(length, array) {
+	for (var i = 0; i < length; i++) {
+		array.push({ userId: i + 1, employeeName: faker.name.findName(), projectName: "Проект " + i, timeMinutes: SetRandomNumber(60, 360) })
+	}
+}
+function SetRandomNumber(minimum, maximum) {
+	return Math.floor(Math.random() * (maximum - minimum)) + minimum;
+}
+function CreateElementOption(listData) {
+	for (var i = 0; i < listData.length; i++) {
+		var opt = document.createElement('option')
+		opt.innerHTML = listData[i].employeeName
+		document.getElementById('names-employees').appendChild(opt)
+	}
+}
+
+function SendReport() {
+	document.getElementById('box-table').innerHTML = '' 
+	var employeeName = document.getElementById('names-employees').value
+	document.getElementById('head-name').innerHTML = employeeName
+	var totalTime = 0
+	for (var i = 0; i < listData.length; i++) {
+		if (listData[i].employeeName === employeeName) {
+
+			var boxLine = document.createElement('div')
+			boxLine.setAttribute('class', 'line-table-worker')
+
+			var blockWorker = document.createElement('div')
+			blockWorker.setAttribute('class', 'name-worker')
+			blockWorker.innerHTML = listData[i].projectName
+
+			var blockTime = document.createElement('div')
+			blockTime.setAttribute('class', 'time-wasted')
+			blockTime.innerHTML = listData[i].timeMinutes
+
+			boxLine.appendChild(blockWorker)
+			boxLine.appendChild(blockTime)
+			document.getElementById('box-table').appendChild(boxLine)
+			totalTime += listData[i].timeMinutes
+		}
+	}
+	document.getElementById('total-time').innerHTML = totalTime
+}
+
